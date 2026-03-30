3e9339b3ee73a47163ee34163446ee1985d8ad10
Tanay Sachdeva <151042948+tanay-io@users.noreply.github.com>
2026-03-30 19:11:00 +0530

    Delete dist directory

--- dist/executors/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.executorRegistry = void 0;
-const http_executor_1 = require("./http.executor");
-exports.executorRegistry = {
-    http: http_executor_1.httpExecutor,
-};
